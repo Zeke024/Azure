e4e0fc2072cea4b7460e376fe7b003bbeb101a59
icmedev <shivanand69420@gmail.com>
2024-04-10 22:10:29 +0700

    feat: add hohohub-button.ts (i forgot)

---
+++ src/events/interactionCreate/hohohub-button.ts
@@ -0,0 +1,24 @@
+import { ActionRowBuilder, ButtonInteraction, ModalActionRowComponentBuilder, ModalBuilder, TextInputBuilder, TextInputStyle } from "discord.js";
+
+export default async (interaction: ButtonInteraction) => {
+  if (!interaction.isButton()) return;
+  if (interaction.customId !== "BYPASS@HOHOHUB") return;
+
+  const modal = new ModalBuilder()
+    .setTitle("Hohohub bypasser")
+    .setCustomId("HOHOHUB_MODAL")
+    .addComponents(
+      new ActionRowBuilder<ModalActionRowComponentBuilder>().addComponents(
+        new TextInputBuilder()
+          .setLabel("Hohohub link")
+          .setStyle(TextInputStyle.Short)
+          .setCustomId("HOHOHUB_LINK")
+          .setPlaceholder("Your Hohohub key link here...")
+          .setRequired(true)
+          .setMinLength(1)
+          .setMaxLength(4000)
+      )
+    );
+
+  await interaction.showModal(modal);
+};
